1ca2b7c74eb6e7089b47bb1674320810ba308ef7
AdmiralKid <siddhharth47@gmail.com>
2022-02-02 21:02:45 +0530

    UI enhanchment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,40 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { TodoEffects } from './store/todoStore/todo.effects';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [
     AppComponent,
     TodolistComponent,
-    CreatetodoComponent
+    CreatetodoComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({todos: todoReducer}, {}),
+    StoreModule.forRoot({ todos: todoReducer }, {}),
     ReactiveFormsModule,
     HttpClientModule,
-    EffectsModule.forRoot([TodoEffects])
+    EffectsModule.forRoot([TodoEffects]),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/createtodo/createtodo.component.ts
+++ src/app/components/createtodo/createtodo.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Todo } from 'src/app/models/entities/todo.model';
 import { createtodo } from 'src/app/store/todoStore/todo.actions';
@@ -27,12 +28,13 @@ export class CreatetodoComponent implements OnInit {
       let todo: Todo = this.createTodo.value as Todo;
       todo.todoId = uuidv4();
       this.store.dispatch(createtodo(todo));
+      this.router.navigate(['/todolist']);
     }
   }
   get title() {
     return this.createTodo.get('title') as FormControl;
   }
-  constructor(private formBuilder: FormBuilder, private store: Store) {
+  constructor(private formBuilder: FormBuilder, private store: Store,  private router: Router ) {
     this.createTodo = formBuilder.group({
       todoId: [''],
       title: [
